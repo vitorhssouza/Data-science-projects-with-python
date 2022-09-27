f41ae16c67bea9590a8cb27f66bf103c01720d4f
vitor souza <vitorugo_kta@hotmail.com>
2022-09-27 19:52:28 -0300

    Removendo a pasta selenium do projeto

--- projeto_selenium/main.py
+++
@@ -1,9 +0,0 @@
-from projeto_selenium.modulos.funcoes import *
-
-
-def main() -> None:
-    menu()
-
-
-if __name__ == '__main__':
-    main()
--- projeto_selenium/teste.py
+++
@@ -1,8 +0,0 @@
-#from projeto_selenium.classes import *
-from projeto_selenium.modulos.funcoes import *
-
-#site = Automatizar('https://sistemaswebb3-listados.b3.com.br/indexPage/day/IBOV?language=pt-br')
-
-
-
-print(empresas())
